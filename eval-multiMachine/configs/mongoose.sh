0352731c8f20bcb63e6b90278e0c04f842de2028
Rui Gu <rui.gu3@gmail.com>
2015-03-14 13:44:45 -0400

    Modify configuration file for apache and mongoose for non-proxy  mode The client command for non-proxy mode need to be change, including the ip and the port.

--- eval-multiMachine/configs/mongoose.sh
+++ eval-multiMachine/configs/mongoose.sh
@@ -11,7 +11,12 @@ msmr_root_client="/home/ruigu/Workspace/m-smr"        # root dir for m-smr
 msmr_root_server="/home/ruigu/SSD/m-smr"
 input_url="127.0.0.1"                                 # url for client to query
 
-client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n 10 -c 10 http://128.59.17.172:9000/"
+if [ $proxy -eq 1 ]
+then
+    client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n 10 -c 10 http://128.59.17.172:9000/"
+else
+    client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n 10 -c 10 http://128.59.17.174:7000/"
+fi
                                                       # command to start the clients
 server_cmd="'${msmr_root_server}/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 2 '"
                                                       # command to start the real server

2a9e84e5fbcc0730f591cabb8b8579360786c7fa
Heming Cui <heming@bug01.cs.columbia.edu>
2015-03-16 13:43:52 -0400

    Nits for mongodb script on bug01.

--- apps/mongodb/sysbench-mongodb/run.simple.bash
+++ apps/mongodb/sysbench-mongodb/run.simple.bash
@@ -26,23 +26,24 @@ PORT=7000
 IP=127.0.0.1
 
 cd $MSMR_ROOT/apps/mongodb/sysbench-mongodb
-rm -rf db-dir
-mkdir db-dir
+#rm -rf db-dir
+mkdir -p db-dir
 killall -9 mongo mongod
 
 echo "Starting mongodb server..."
 $MSMR_ROOT/apps/mongodb/install/bin/mongod --port $PORT --dbpath=$PWD/db-dir --quiet &> mongodb.log &
-sleep 3;
+sleep 15;
 
 echo "Preparing for database sbtest..."
 $MSMR_ROOT/apps/mongodb/install/bin/mongo --port $PORT --host $IP < cleanup.js
 sleep 1;
 killall -9 mongo mongod
+#exit 0;
 
 echo "Restarting mongodb server..."
+LD_PRELOAD=$XTERN_ROOT/dync_hook/interpose.so \
 $MSMR_ROOT/apps/mongodb/install/bin/mongod --port $PORT --dbpath=$PWD/db-dir --quiet &> mongodb.log &
-sleep 3;
-
+sleep 15;
 
 
 

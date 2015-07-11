9c9747f09bd33f99ccc1cad681a7b20d47b03366
heming@cs.columbia.edu <heming@cs.columbia.edu>
2015-07-11 05:02:41 -0400

    added more tests

--- eval-multiMachine/configs/new-mongoose.sh
+++ eval-multiMachine/configs/new-mongoose.sh
@@ -2,8 +2,8 @@
 app="mg-server"                                       # app name appears in process list
 xtern=1                                               # 1 use xtern, 0 otherwise
 proxy=1                                               # 1 use proxy, 0 otherwise
-sch_paxos=0                                           # 1 xtern will schedule with paxos, 0 otherwise
-sch_dmt=0                                             # 1 libevent_paxos will schedule with DMT, 0 otherwise
+sch_paxos=1                                           # 1 xtern will schedule with paxos, 0 otherwise
+sch_dmt=1                                             # 1 libevent_paxos will schedule with DMT, 0 otherwise
 leader_elect=0                                        # 1 enable leader election demo, 0 otherwise
 checkpoint=0                                          # 1 use checkpoint on relicas, 0 otherwise
 checkpoint_period=5                                   # period of CRIU checkpoint, e.g. 10 seconds
@@ -23,5 +23,5 @@ else
     client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n 128 -c 8 http://128.59.17.174:7000/test.php"
 fi
                                                       # command to start the clients
-server_cmd="'ulimit -c unlimited && ${msmr_root_server}/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 8 &'"
+server_cmd="'${msmr_root_server}/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 8 &'"
                                                       # command to start the real server

f4760fd1abed4e6f0e38d245c5c5e78080cd9f15
heming@cs.columbia.edu <heming@cs.columbia.edu>
2015-07-13 09:04:20 -0400

    done with clamav

--- eval-multiMachine/configs/clamav.sh
+++ eval-multiMachine/configs/clamav.sh
@@ -13,21 +13,24 @@ sch_dmt=1                                             # 1 libevent_paxos will sc
 leader_elect=0                                        # 1 enable leader election demo, 0 otherwise
 checkpoint=0                                          # 1 use checkpoint on relicas, 0 otherwise
 checkpoint_period=10                                  # period of CRIU checkpoint, e.g. 10 seconds
-msmr_root_client="/home/ruigu/Workspace/m-smr"        # root dir for m-smr
-msmr_root_server="/home/ruigu/SSD/m-smr"
+msmr_root_client=`echo $MSMR_ROOT`                    # root dir for m-smr. this command assumes that curent machine's m-smr path is the same as the servers'.
+msmr_root_server=`echo $MSMR_ROOT`
 input_url="127.0.0.1"                                 # url for client to query
 analysis_tools=""                                     # for executing analysis tools (e.g., analysis_tools="--worker1=helgrind")
 
 if [ $proxy -eq 1 ]
 then
-    client_cmd="parallel-ssh -v -p 1 -i -t 15 -h head 'cd ${msmr_root_server}/apps/clamav/ && LD_PRELOAD=${msmr_root_server}/libevent_paxos/client-ld-preload/libclilib.so ./run-client 9000 '"
+    client_cmd="parallel-ssh -v -p 1 -i -t 15 -h head 'cd ${msmr_root_server}/apps/clamav/ && \
+	LD_PRELOAD=${msmr_root_server}/libevent_paxos/client-ld-preload/libclilib.so \
+	${msmr_root_client}/apps/clamav/install/bin/clamdscan --config-file=./client-eth0-9000.conf \
+	-m ${msmr_root_client}/apps/clamav/install/* '"
 else
-    client_cmd="parallel-ssh -v -p 1 -i -t 15 -h head 'cd ${msmr_root_server}/apps/clamav/ && ./run-client 7000 '"
+    client_cmd="parallel-ssh -v -p 1 -i -t 15 -h head 'cd ${msmr_root_server}/apps/clamav/ && \
+	${msmr_root_client}/apps/clamav/install/bin/clamdscan --config-file=./client-lo-7000.conf \
+        -m ${msmr_root_client}/apps/clamav/install/* '"
 fi
-                                                      # command to start the clients
+
 # We didn't use start-server script because we have to LD_PRELOAD for the whole scripts.
 # Thus, server.conf will be corrupted by the xtern stderr(Init share memory stuff.)
-
-#server_cmd="'cd ${msmr_root_server}/apps/clamav && ./start-server 7000 '"
-server_cmd="'cd ${msmr_root_server}/apps/clamav && ./install/sbin/clamd --config-file=./server.conf &> out.txt & '"
-                                                      # command to start the real server
+server_cmd="'${msmr_root_server}/apps/clamav/install/sbin/clamd \
+	--config-file=${msmr_root_server}/apps/clamav/client-lo-7000.conf &> out.txt & '"

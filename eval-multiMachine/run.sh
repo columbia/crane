84ba65cbffc17e56a9c0616337398fa750903815
heming <heming@cs.hku.hk>
2015-07-15 17:59:41 +0800

    nits.

--- eval-multiMachine/run.sh
+++ eval-multiMachine/run.sh
@@ -2,7 +2,24 @@
 
 # This is the starter file of the whole experiment.
 
-source configs/new-mongoose-helgrind-worker1.sh
+if [ ! $1 ];
+then
+        echo "Usage: $0 <application cfg file>"
+        echo "$0 $MSMR_ROOT/eval-multiMachine/configs/mongoose.sh"
+        exit 1;
+fi
+
+source $1;
+build_project="true";
+if [ $2"X" != "X" ];
+then
+	if [ $2 == "no_build" ];
+	then
+	        build_project="false";
+	fi
+fi
+
+#source configs/new-mongoose-helgrind-worker1.sh
 #source configs/new-mongoose.sh
 #source configs/mongoose.sh
 #source configs/apache.sh
@@ -14,10 +31,13 @@ source configs/new-mongoose-helgrind-worker1.sh
 #source configs/clamav.sh
 #source configs/heming-mediatomb.sh
 
-# Update worker-build.py to the server
-scp worker-build.py bug03.cs.columbia.edu:~/
-scp worker-build.py bug01.cs.columbia.edu:~/
-scp worker-build.py bug02.cs.columbia.edu:~/
+if [ $build_project == "true" ];
+then
+	# Update worker-build.py to the server
+	scp worker-build.py bug03.cs.columbia.edu:~/
+	scp worker-build.py bug01.cs.columbia.edu:~/
+	scp worker-build.py bug02.cs.columbia.edu:~/
+fi
 
 # Update worker-run.py to the server
 scp worker-run.py bug03.cs.columbia.edu:~/
@@ -31,5 +51,5 @@ scp criu-cr.py bug02.cs.columbia.edu:~/
         -k ${checkpoint} -t ${checkpoint_period} \
         -c ${msmr_root_client} -s ${msmr_root_server} \
         --sp ${sch_paxos} --sd ${sch_dmt} \
-        --scmd "${server_cmd}" --ccmd "${client_cmd}" -b "true" ${analysis_tools}
+        --scmd "${server_cmd}" --ccmd "${client_cmd}" -b ${build_project} ${analysis_tools}
 

39f4cd5d353fae4a7340c34b29513c20d708ec30
heming <heming@cs.hku.hk>
2015-08-04 03:02:27 +0800

    done with checkpiont!

--- eval-multiMachine/checkpoint-restore.sh
+++ eval-multiMachine/checkpoint-restore.sh
@@ -12,7 +12,7 @@ OP=$1
 PROG_NAME=$2
 DIR=$3
 CONTAINER="u1"
-CRIU_ARGS="--shell-job --tcp-established --file-locks"
+CRIU_ARGS=" --shell-job --tcp-established --file-locks"
 SNAPS_DIR="/var/lib/lxc/$CONTAINER/snaps"
 
 echo "Running command: $0 $OP $PROG_NAME $DIR"
@@ -39,16 +39,19 @@ if [ "$OP" == "checkpoint" ]; then
 
 # Resume process and the container.
 	sudo lxc-start -n $CONTAINER
+	sleep 1
+	ssh -t $USER@$(sudo lxc-info -i -H -n $CONTAINER) "tmux start-server; tmux new-session -d -s tmux_session"
+	ssh -t $USER@$(sudo lxc-info -i -H -n $CONTAINER) "tmux send-keys -t tmux_session \"sudo criu restore -d -D $HOME/$DIR $CRIU_ARGS\" C-m"
 	sudo lxc-attach -n $CONTAINER -- sudo criu restore -d -D $HOME/$DIR $CRIU_ARGS
 	PID=`sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME | awk '{print $1}'`
 	echo "Process pid $PID has been successfully checkpointed with its file system at checkpoint-$PID.tar.gz in current directory."
 fi
 
-if [ "$OP" != "restore" ]; then
+#if [ "$OP" != "restore" ]; then
 # First, checkpoint the file system of the container.
 
 # Second, restore the server application within the container, and bdb storage of the proxy process.
 #sudo criu restore -d -D $DIR                         $CRIU_ARGS
 
-fi
+#fi
 

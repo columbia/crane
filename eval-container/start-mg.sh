e90de2c1f88e536af3562b96362dc628766fe6a0
heming <heming@cs.columbia.edu>
2015-08-11 04:34:12 +0800

    added lxc in worker-build. worked

---
+++ eval-container/start-mg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONTAINER="u1"
+CONTAINER_IP="10.0.3.111"
+CMD="/home/heming/hku/m-smr/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 1 &"
+PROG_NAME="mg-server"
+
+sudo lxc-stop -n $CONTAINER
+sudo lxc-start -n $CONTAINER
+sleep 5
+ssh -t $USER@$CONTAINER_IP "tmux start-server; tmux new-session -d -s tmux_session"
+ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"$CMD\" C-m"
+PID=`sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME | awk '{print $1}'`
+echo $PID

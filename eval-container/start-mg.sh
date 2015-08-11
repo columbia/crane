5edda0377f24ab3c9c82c0598047967769b6b6db
heming <heming@cs.columbia.edu>
2015-08-11 16:53:04 +0800

    added lxc in worker-build. worked

--- eval-container/start-mg.sh
+++ eval-container/start-mg.sh
@@ -7,7 +7,7 @@ PROG_NAME="mg-server"
 sudo lxc-stop -n $CONTAINER
 sudo lxc-start -n $CONTAINER
 sleep 5
-ssh -t $USER@$CONTAINER_IP "tmux start-server; tmux new-session -d -s tmux_session"
-ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"$CMD\" C-m"
+ssh $USER@$CONTAINER_IP "tmux start-server; tmux new-session -d -s tmux_session"
+ssh $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"$CMD\" C-m"
 PID=`sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME | awk '{print $1}'`
 echo $PID

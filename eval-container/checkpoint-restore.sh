e8e5b32842d0c9da4e70078970e01c37132c8d12
heming <heming@cs.columbia.edu>
2015-08-11 17:28:43 +0800

    added lxc in worker-build. worked

--- eval-container/checkpoint-restore.sh
+++ eval-container/checkpoint-restore.sh
@@ -35,8 +35,7 @@ if [ "$OP" == "checkpoint" ]; then
 	sudo lxc-attach -n $CONTAINER -- sudo rm -rf $HOME/$DIR
 	sudo lxc-attach -n $CONTAINER -- mkdir $HOME/$DIR
 	ssh -t $USER@$CONTAINER_IP "tmux start-server; tmux new-session -d -s tmux_session"
-	ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"sudo criu dump -t $PID -D $HOME/$DIR $CRIU_ARGS\" C-m"
-	#sudo lxc-attach -n $CONTAINER -- sudo criu dump -D $HOME/$DIR -t $PID $CRIU_ARGS
+	ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"sudo criu dump -t $PID -D $HOME/$DIR $CRIU_ARGS &> /tmp/dump.txt\" C-m"
 	#exit 0
 
 # Second, checkpoint the file system of the container, and bdb storage of the proxy process.
@@ -54,38 +53,73 @@ if [ "$OP" == "checkpoint" ]; then
 # Resume process and the container.
 	sudo lxc-start -n $CONTAINER
 	sleep 10
+	i=1
 	ssh -t $USER@$CONTAINER_IP "tmux start-server; tmux new-session -d -s tmux_session"
-	ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"sudo criu restore -d -D $HOME/$DIR $CRIU_ARGS\" C-m"
-	#sudo lxc-attach -n $CONTAINER -- sudo criu restore -d -D $HOME/$DIR $CRIU_ARGS
+	while [ $i -le 5 ]
+	do
+		echo "Restoring process checkpoint in $DIR for the $i time (may need 2 times)..."
+		(( i++ ))
+		ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"sudo criu restore -d -D $HOME/$DIR $CRIU_ARGS &> /tmp/restore.txt\" C-m"
+		RES=`sudo lxc-attach -n $CONTAINER -- cat /tmp/restore.txt | grep Error`
+		if [ "$RES"X == "X" ]; then
+			break
+		fi
+	done
 	PID=`sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME | awk '{print $1}'`
-	echo "Process pid $PID has been successfully checkpointed with its file system at checkpoint-$PID.tar.gz in current directory."
-       echo "To make sure checkpoint of the process succeeded, run this command below. It should contain a few *.img files."
-       echo "> sudo lxc-attach -n $CONTAINER -- ls -l $HOME/$DIR"
+	if [ "$PID"X == "X" ]; then
+		echo "Process has been failed to checkpointed, please contact developers."
+	else
+
+		echo "Process pid $PID has been successfully checkpointed with its file system at checkpoint-$PID.tar.gz in current directory."
+		echo ""
+		echo "To double check checkpoint of the process succeeded, run this command below. It should contain a few *.img files."
+		echo "> sudo lxc-attach -n $CONTAINER -- ls -l $HOME/$DIR"
+		echo ""
+		echo "To double check the process $PID is still running in lxc, run this command below."
+		echo "> sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME"
+	fi
 fi
 
 if [ "$OP" == "restore" ]; then
-      tar zxvf $DIR
-      echo "Restoring .db directory to $HOME/.db in the host OS..."
-      rm $HOME/.db -rf
-      mv db $HOME/.db
-      echo "Restoring $HOME directory file system in the lxc container..."
-      cp filesystem-checkpoint.patch $HOME
-      sudo chmod 666 $HOME/filesystem-checkpoint.patch
+	tar zxvf $DIR
+	echo "Restoring .db directory to $HOME/.db in the host OS..."
+	rm $HOME/.db -rf
+	mv db $HOME/.db
+	echo "Restoring $HOME directory file system in the lxc container..."
+	cp filesystem-checkpoint.patch $HOME
+	sudo chmod 666 $HOME/filesystem-checkpoint.patch
 
 # Second, restore the file system of the container.
-      sudo lxc-stop -n $CONTAINER
-      sleep 1
-      echo "Restoring base file system in $FS_BASE_DIR, may take a few minutes..."
-      sudo lxc-snapshot -n $CONTAINER -r base
-      echo "Restoring current file system to the checkpoint moment, may take a few seconds..."
-      sudo patch -d $SNAPS_DIR/../rootfs/$HOME -p1 < $HOME/filesystem-checkpoint.patch
+	sudo lxc-stop -n $CONTAINER
+	sleep 1
+	echo "Restoring base file system in $FS_BASE_DIR, may take a few minutes..."
+	sudo lxc-snapshot -n $CONTAINER -r base
+	echo "Restoring current file system to the checkpoint moment, may take a few seconds..."
+	sudo patch -d $SNAPS_DIR/../rootfs/$HOME -p1 < $HOME/filesystem-checkpoint.patch
 
 # Resume process and the container.
 	sudo lxc-start -n $CONTAINER
 	sleep 10
+	i=1
 	ssh -t $USER@$CONTAINER_IP "tmux start-server; tmux new-session -d -s tmux_session"
-	ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"sudo criu restore -d -D $HOME/checkpoint $CRIU_ARGS\" C-m"
+	while [ $i -le 5 ]
+	do
+		echo "Restoring process checkpoint in $DIR for the $i time (may need 2 times)..."
+		(( i++ ))
+		ssh -t $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"sudo criu restore -d -D $HOME/checkpoint $CRIU_ARGS &> /tmp/restore.txt\" C-m"
+		RES=`sudo lxc-attach -n $CONTAINER -- cat /tmp/restore.txt | grep Error`
+		if [ "$RES"X == "X" ]; then
+			break
+		fi
+	done
 	PID=`sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME | awk '{print $1}'`
-	echo "Process pid $PID has been successfully checkpointed with its file system at checkpoint-$PID.tar.gz in current directory."
+	if [ "$PID"X == "X" ]; then
+		echo "Process has been failed to restored from $DIR, please contact developers."
+	else
+		echo "Process pid $PID has been successfully restored with its file system at checkpoint-$PID.tar.gz in current directory."
+		echo ""
+		echo "To double check the process $PID is restored and running in lxc, run this command below."
+		echo "> sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME"
+	fi
 fi
 
--- eval-container/start-mg.sh
+++ eval-container/start-mg.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
+
 CONTAINER="u1"
 CONTAINER_IP="10.0.3.111"
-CMD="/home/heming/hku/m-smr/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 1 &"
-PROG_NAME="mg-server"
+EX_PROG_NAME="mg-server"
+EX_CMD="/home/heming/hku/m-smr/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 1 &"
+if [ "$#" != "2" ]; then
+	echo "Illegal number of parameters."
+       echo "Usage: $0 <server program name> <command to run the server in container $CONTAINER>"
+       echo "$0 $EX_PROG_NAME \"$EX_CMD\""
+       exit 1;
+fi
+PROG_NAME=$1
+CMD=$2
+echo ""
+echo "Starting the server $PROG_NAME in lxc $CONTAINER, with IP $USER@$CONTAINER_IP, \
+	with command: \"$CMD\""
 
 sudo lxc-stop -n $CONTAINER
 sudo lxc-start -n $CONTAINER
@@ -10,4 +23,11 @@ sleep 5
 ssh $USER@$CONTAINER_IP "tmux start-server; tmux new-session -d -s tmux_session"
 ssh $USER@$CONTAINER_IP "tmux send-keys -t tmux_session \"$CMD\" C-m"
 PID=`sudo lxc-attach -n $CONTAINER -- ps -e | grep $PROG_NAME | awk '{print $1}'`
-echo $PID
+echo "$PROG_NAME with pid ($PID) is started. If pid is empty, contact developers."
+echo ""
+echo "You can run the command to checkpoint the process."
+echo "> ./checkpoint-restore.sh checkpoint $PROG_NAME ./checkpoint"
+echo ""
+echo "If checkpoint succeeds, you can run the command to restore the process."
+echo "> ./checkpoint-restore.sh restore $PROG_NAME checkpoint-$PID.tar.gz"
+

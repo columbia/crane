291988310921de596a837000bb771ca62ee91014
heming@cs.columbia.edu <heming@cs.columbia.edu>
2015-08-16 14:32:36 -0400

    nits.

---
+++ eval-container/collect-scheds.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+rm -rf scheds
+mkdir scheds
+scp bug03.cs.columbia.edu:/dev/shm/dmt_out/serializer-light-pid-*.log scheds/bug03.log
+scp bug01.cs.columbia.edu:/dev/shm/dmt_out/serializer-light-pid-*.log scheds/bug01.log
+scp bug02.cs.columbia.edu:/dev/shm/dmt_out/serializer-light-pid-*.log scheds/bug02.log

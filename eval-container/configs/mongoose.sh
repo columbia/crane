ae6916e7b936e15dbd1cb1c19d79fd98ce6b8426
heming <heming@cs.columbia.edu>
2015-08-21 19:36:38 +0800

    nits for application options, cleanup

--- eval-container/configs/mongoose.sh
+++ eval-container/configs/mongoose.sh
@@ -1,28 +1,33 @@
 # Setups for mongoose
+source configs/default-options.sh
 app="mg-server"                                       # app name appears in process list
-xtern=0                                               # 1 use xtern, 0 otherwise
+xtern=1                                               # 1 use xtern, 0 otherwise
 proxy=1                                               # 1 use proxy, 0 otherwise
-sch_paxos=0                                           # 1 xtern will schedule with paxos, 0 otherwise
-sch_dmt=0                                             # 1 libevent_paxos will schedule with DMT, 0 otherwise
-leader_elect=1                                        # 1 enable leader election demo, 0 otherwise
+sch_paxos=1                                           # 1 xtern will schedule with paxos, 0 otherwise
+sch_dmt=1                                             # 1 libevent_paxos will schedule with DMT, 0 otherwise
+leader_elect=0                                        # 1 enable leader election demo, 0 otherwise
 checkpoint=0                                          # 1 use checkpoint on relicas, 0 otherwise
 checkpoint_period=5                                   # period of CRIU checkpoint, e.g. 10 seconds
-msmr_root_client="/home/ruigu/Workspace/m-smr"        # root dir for m-smr
-msmr_root_server="/home/ruigu/SSD/m-smr"
+msmr_root_client=`echo $MSMR_ROOT`        # root dir for m-smr
+msmr_root_server=`echo $MSMR_ROOT`
 input_url="127.0.0.1"                                 # url for client to query
 analysis_tools=""                                     # for executing analysis tools (e.g., analysis_tools="--worker1=helgrind")
+num_req=128
+#num_req=256
+#num_req=512 # added lxc, robust on these workloads.
+num_thd=8
 
 if [ $proxy -eq 1 ]
 then
     if [ $leader_elect -eq 1 ]
     then
-        client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n 128 -c 8 http://128.59.17.174:9000/test.php"
+        client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n ${num_req} -c ${num_thd} http://128.59.17.174:9000/test.php"
     else
-        client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n 128 -c 8 http://128.59.17.174:9000/test.php"
+        client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n ${num_req} -c ${num_thd} http://128.59.17.174:9000/test.php"
     fi
 else
-    client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n 128 -c 8 http://128.59.17.174:7000/test.php"
+    client_cmd="${msmr_root_client}/apps/apache/install/bin/ab -n ${num_req} -c ${num_thd} http://128.59.17.174:7000/test.php"
 fi
                                                       # command to start the clients
-server_cmd="'${msmr_root_server}/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 8 &'"
+server_cmd="'${msmr_root_server}/apps/mongoose/mg-server -I /usr/bin/php-cgi -p 7000 -t 8 &>./server-out.txt &'"
                                                       # command to start the real server

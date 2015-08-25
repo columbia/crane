27448624f6837e48521c8c82ad7fe0f3938735d7
Tianyu Chen <chentianyu@outlook.com>
2015-08-25 04:15:35 -0400

    new run all

--- eval-container/run-all.sh
+++ eval-container/run-all.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-rm *.log
-rounds = 3
+# rm *.log
+rounds = 11
 
 ./new-run.sh configs/mongoose.sh no_build joint_sched $rounds
 ./new-run.sh configs/mongoose.sh no_build separate_sched $rounds
-# ./new-run.sh configs/mongoose.sh no_build xtern_only 5
+./new-run.sh configs/mongoose.sh no_build xtern_only $rounds
 ./new-run.sh configs/mongoose.sh no_build proxy_only $rounds
-# ./new-run.sh configs/mongoose.sh no_build orig 5
+./new-run.sh configs/mongoose.sh no_build orig $rounds
 
+./new-run.sh configs/apache.sh no_build joint_sched $rounds
+./new-run.sh configs/apache.sh no_build separate_sched $rounds
+./new-run.sh configs/apache.sh no_build xtern_only $rounds
+./new-run.sh configs/apache.sh no_build proxy_only $rounds
+./new-run.sh configs/apache.sh no_build orig $rounds
+
+./new-run.sh configs/clamav.sh no_build joint_sched $rounds
+./new-run.sh configs/clamav.sh no_build separate_sched $rounds
+./new-run.sh configs/clamav.sh no_build xtern_only $rounds
+./new-run.sh configs/clamav.sh no_build proxy_only $rounds
+./new-run.sh configs/clamav.sh no_build orig $rounds
+
+./new-run.sh configs/mediatomb.sh no_build joint_sched $rounds
+./new-run.sh configs/mediatomb.sh no_build separate_sched $rounds
+./new-run.sh configs/mediatomb.sh no_build xtern_only $rounds
+./new-run.sh configs/mediatomb.sh no_build proxy_only $rounds
+./new-run.sh configs/mediatomb.sh no_build orig $rounds
 ./grab.py

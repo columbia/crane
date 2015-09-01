40492ae6a0ae359d8ea5f0225dcc71be9299a972
Tianyu Chen <chentianyu@outlook.com>
2015-09-01 17:21:52 -0400

    add usleep time eval script

--- eval-container/change_sched_with_paxos_usleep.sh
+++ eval-container/change_sched_with_paxos_usleep.sh
@@ -1,6 +1,8 @@
-mv worker-run.py worker-run-orig.py
+#!/bin/bash
+
+rm worker-run.py
 cp worker-run-orig.py worker-run.py
 
 # test 1/10/1000/10000
-usleep_val=1
+usleep_val=$1
 sed -i "49i \ \ \ \ os.system(\"sed -i -e 's/sched_with_paxos_usleep = [0-9]\\\+/sched_with_paxos_usleep  = $usleep_val/g' local.options\")" worker-run.py
